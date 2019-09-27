fcc719825e7e36dd025710b6823a6b8655ebcd0b
Jake Maguy <jakemaguy@gmail.com>
2019-09-26 20:13:54 -0400

    Section 2: Lesson 20 (Adding in-line and external CSS)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // Templates should be used when html gets over 3 lines or so
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
