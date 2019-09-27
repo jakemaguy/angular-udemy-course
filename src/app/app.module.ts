126a331cc36a053a1f2db0fdf1ddee7087431126
Jake Maguy <jakemaguy@gmail.com>
2019-09-26 20:01:20 -0400

    Section 2: Lesson 17 (Created custom component without use of angular CLI)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
+// NGModule declarations for angular app
 @NgModule({
+  // Custom Components go into the Declarations section
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
