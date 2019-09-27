e5bbac2a2414852999c62fad2bdb68dc0b525cb0
Jake Maguy <jakemaguy@gmail.com>
2019-09-26 20:04:42 -0400

    Section 2: Lesson 18 (Created and inserted component using angular CLI)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 // NGModule declarations for angular app
 @NgModule({
   // Custom Components go into the Declarations section
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
