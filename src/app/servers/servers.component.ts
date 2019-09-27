171f804d504aa106b999156575dca7f8419be3b2
Jake Maguy <jakemaguy@gmail.com>
2019-09-27 19:50:57 -0400

    Section 2: Lesson 38 (Introduction to dynamic css styling with ngStyle)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,7 +4,12 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // Templates should be used when html gets over 3 lines or so
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  // adding a css section for ngClass to reference to, in a specified scenerio
+  styles: [`
+  .online {
+    color: white;
+  }`]
 })
 export class ServersComponent implements OnInit {
   serverId = 10;
