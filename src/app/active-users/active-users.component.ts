719636479c34055049c507edb5f9ee2dd76cc1f3
Jake Maguy <jakemaguy@gmail.com>
2019-10-19 13:06:58 -0400

    Instructor Solution

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -6,12 +6,14 @@ import { UsersService } from '../users.service';
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
-  // @Output() userSetToInactive = new EventEmitter<number>();
-
+export class ActiveUsersComponent implements OnInit {
+  users: string[];
   constructor(private usersService: UsersService) { }
   onSetToInactive(id: number) {
     this.usersService.onSetToInactive(id);
+  }
+
+  ngOnInit() {
+    this.users = this.usersService.activeUsers;
   }
 }
--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -6,13 +6,14 @@ import { UsersService } from '../users.service';
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  // @Output() userSetToActive = new EventEmitter<number>();
-
+export class InactiveUsersComponent implements OnInit {
+  users: string[];
   constructor(private usersService: UsersService) { }
 
   onSetToActive(id: number) {
     this.usersService.onSetToActive(id);
+  }
+  ngOnInit() {
+    this.users = this.usersService.inactiveUsers;
   }
 }
