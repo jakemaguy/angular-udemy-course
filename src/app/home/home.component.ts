fabc8b45b8ade7c7519cbcbe57c913d7af41606c
Jake Maguy <jakemaguy@gmail.com>
2019-10-20 16:15:48 -0400

    Lesson 135:  Programatic Query Parameters

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,9 +13,10 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
-    // Router path is relative
-    this.router.navigate(['/servers']);
+  onLoadServer(id: number) {
+    // Query parameters programatic approach
+    this.router.navigate(['/servers', id, 'edit'], {queryParams:
+      {allowEdit: '1', fragment: 'loading'}});
   }
 
 }
